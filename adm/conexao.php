c18ebe2ab2c123c3ff66a8504c18379af5a95e91
Caio <caio.scatolino@outlook.com>
2023-04-03 18:32:32 -0300

    criando conexão com bd e validando login

--- adm/conexao.php
+++ adm/conexao.php
@@ -1,3 +1,13 @@
 <?php
 
-?>
+try {
+    $pdo = new PDO(
+        "mysql:host=localhost;dbname=novaescola",
+        "root",
+        ""
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexão bem-sucedida!";
+} catch (PDOException $e) {
+    echo "Falha na conexão: " . $e->getMessage();
+}
--- valida_login.php
+++ valida_login.php
@@ -1,4 +1,34 @@
 <?php
-echo "Valida login Aberto";
 
+if(isset($_POST['acao'])) {
+
+// importando arquivo conexão
+
+    require_once "adm/conexao.php"; 
+
+// obtendo valores digitados no form
+
+    $usuario = $_POST['usuario'];
+    $senha   = $_POST['senha'];
+
+// verificar se existe no banco de dados
+
+    $sql = "SELECT * FROM pessoas WHERE pessoas.cpf = :login and pessoas.senha = :senha";
+    $comando = $pdo -> prepare($sql);
+
+    $comando -> bindParam(':login', $usuario);
+    $comando -> bindParam(':senha', $senha);
+    $comando -> execute();
+
+    if($comando -> rowCount() === 1) {
+        echo "Existe um usuário em nossa base de dados.";
+        echo $usuario;
+    } else {
+        echo "Não existe um usuário em nossa base de dados.";
+    };
+
+
+}else {
+    echo 'Erro... Acesso indevido!';
+}
 ?>
