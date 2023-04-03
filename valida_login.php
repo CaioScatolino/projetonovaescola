e242cfc5630150846904cecf85d379976031b430
Caio <caio.scatolino@outlook.com>
2023-04-03 18:39:43 -0300

    criptografando senha

--- valida_login.php
+++ valida_login.php
@@ -10,25 +10,30 @@ if(isset($_POST['acao'])) {
 
     $usuario = $_POST['usuario'];
     $senha   = $_POST['senha'];
+    $senha_hash = hash('sha256', $senha);
 
 // verificar se existe no banco de dados
 
-    $sql = "SELECT * FROM pessoas WHERE pessoas.cpf = :login and pessoas.senha = :senha";
+    $sql = "SELECT * FROM pessoas WHERE pessoas.cpf = :login and pessoas.senha = :senha_hash";
     $comando = $pdo -> prepare($sql);
 
     $comando -> bindParam(':login', $usuario);
-    $comando -> bindParam(':senha', $senha);
+    $comando -> bindParam(':senha_hash', $senha_hash);
     $comando -> execute();
 
     if($comando -> rowCount() === 1) {
         echo "Existe um usuário em nossa base de dados.";
-        echo $usuario;
+        echo $usuario."<br>";
+        echo $senha."<br>";
+        echo $senha_hash."<br>";
     } else {
         echo "Não existe um usuário em nossa base de dados.";
+        echo $usuario . "<br>";
+        echo $senha . "<br>";
+        echo $senha_hash . "<br>";
     };
 
 
 }else {
     echo 'Erro... Acesso indevido!';
 }
-?>
