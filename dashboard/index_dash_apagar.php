df43778ac3efd89cafdf0435230bdce2fc58d7f0
Caio <caio.scatolino@outlook.com>
2023-04-04 21:03:19 -0300

    adicionando datable para formatação da tabela***

--- dashboard/index_dash_apagar.php
+++ dashboard/index_dash_apagar.php
@@ -18,13 +18,14 @@ $comando->execute();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listar PHP</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
 </head>
 
 <body>
     <h1>Listar registros da Base de Dados</h1>
     <a href="cadastrar.php">Adicionar novo registro</a><br><br>
-    <table class="container table table-dark table-hover text-center">
+    <table id="minhaTabela" class="table table-hover">
         <thead class="table-dark">
             <tr>
                 <th>#ID</th>
@@ -63,7 +64,51 @@ $comando->execute();
 
         </tbody>
     </table>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
 </body>
+<script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+
+
+<!-- DataTables JS + botões de exportação -->
+
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.2.3/css/buttons.dataTables.min.css">
+<script src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.print.min.js"></script>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#minhaTabela').DataTable({
+            "language": {
+                "url": "http://cdn.datatables.net/plug-ins/1.13.4/i18n/pt-BR.json"
+            },
+            dom: 'Bfrtip',
+            // "buttons": [
+            //     'copy',
+            //     'csv',
+            //     'excel',
+            //     'pdf',
+            //     'print'
+            // ],
+            paging: true,
+            buttons: {
+                buttons: [
+                    {
+                        extend: 'pdf', orientation: 'landscape'
+                    },
+                    {
+                        extend: 'excel'
+                    }
+                ]
+            }
+        });
+    });
+</script>
 
 </html>
